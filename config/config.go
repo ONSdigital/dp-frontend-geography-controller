9fa6c46d7ee579b2502c65998e9b07e45d65fa52
Richard-95 <richillwest@gmail.com>
2018-08-31 14:38:08 +0100

    update geo handler

--- config/config.go
+++ config/config.go
@@ -6,15 +6,16 @@ var cfg Config
 
 //Config represents service configuration for dp-frontend-geography-controller
 type Config struct {
-	BindAddr           string `env:"BIND_ADDR"`
-	RendererURL        string `env:"RENDERER_URL"`
-	DownloadServiceURL string `env:"DOWNLOAD_SERVICE_URL"`
+	BindAddr        string `env:"BIND_ADDR"`
+	RendererURL     string `env:"RENDERER_URL"`
+	CodeListsAPIURL string `env:"CODELISTS_API_URL"`
 }
 
 func init() {
 	cfg = Config{
-		BindAddr:    ":23700",
-		RendererURL: "http://localhost:20010",
+		BindAddr:        ":23700",
+		RendererURL:     "http://localhost:20010",
+		CodeListsAPIURL: "http://localhost:22400",
 	}
 	err := gofigure.Gofigure(&cfg)
 	if err != nil {
