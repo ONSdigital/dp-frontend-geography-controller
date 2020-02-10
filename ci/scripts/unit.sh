e1a7c5177d7b920eaf04067ebc5969cb74ad852f
nshumoogum <nathan.shumoogum@methods.co.uk>
2020-02-10 11:11:42 +0000

    Update nomad plans and ci builds

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
-export GOPATH=$(pwd)/go
+cwd=$(pwd)
 
-pushd $GOPATH/src/github.com/ONSdigital/dp-frontend-geography-controller
+pushd $cwd/dp-frontend-geography-controller
   make test
 popd
